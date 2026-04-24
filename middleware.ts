17c2dee08293dfbe45057bec83cac49d2e22fe07
draftabgov-star <draftab.gov@gmail.com>
2026-04-24 23:50:01 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/api/horoscope(.*)",
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    if (!auth().userId) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
-});
+export function middleware() {
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/dashboard/:path*", "/api/:path*"],
 };
