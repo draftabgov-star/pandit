b1ae892587a63c364feab2ef6f89c4b42ee2b3f1
draftabgov-star <draftab.gov@gmail.com>
2026-04-26 13:00:34 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
   }
 }
 
